2ebecfb4201847a174679000650a1ba35b14bafd
Davis Clark <davis.clark@whiteboard-it.com>
2011-12-03 15:35:37 -0600

    changed plugin to pull indent character and length info from the sublime settings

--- js_formatter.py
+++ js_formatter.py
@@ -2,9 +2,10 @@ import sublime, sublime_plugin, jsbeautifier
 
 class JsFormatCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		opts = jsbeautifier.default_options();
-		opts.indent_char = " "
-		opts.indent_size = 4
+		settings = self.view.settings()
+		opts = jsbeautifier.default_options()
+		opts.indent_char = " " if settings.get("translate_tabs_to_spaces") else "\t"
+		opts.indent_size = int(settings.get("tab_size")) if opts.indent_char == " " else 1 
 		opts.max_preserve_newlines = 3
 		selection = self.view.sel()[0]
 		replaceRegion = selection if len(selection) > 0 else sublime.Region(0, self.view.size())
