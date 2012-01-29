385cbf39acabf7aa70e62f9d0899128711670ffa
jdc0589 <jdc0589@gmail.com>
2012-01-28 22:46:24 -0600

    Added JsFormat.sublime-settings

--- js_formatter.py
+++ js_formatter.py
@@ -1,18 +1,22 @@
 import sublime, sublime_plugin, jsbeautifier
 
+s = sublime.load_settings("JsFormat.sublime-settings")
+
 class JsFormatCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		settings = self.view.settings()
 
+		print s.get("max_preserve_newlines")
+
 		opts = jsbeautifier.default_options()
 		opts.indent_char = " " if settings.get("translate_tabs_to_spaces") else "\t"
 		opts.indent_size = int(settings.get("tab_size")) if opts.indent_char == " " else 1 
-		opts.max_preserve_newlines = settings.get("jsformat_max_preserve_newlines") or 3
-		opts.preserve_newlines = settings.get("jsformat_preserve_newlines") or True
-		opts.jslint_happy = settings.get("jsformat_jslint_happy") or False
-		opts.brace_style = settings.get("jsformat_brace_style") or "collapse"
-		opts.keep_array_indentation = settings.get("jsformat_keep_array_indentation") or False
-		opts.indent_level = settings.get("jsformat_indent_level") or 0
+		opts.max_preserve_newlines = s.get("max_preserve_newlines") or 3
+		opts.preserve_newlines = s.get("preserve_newlines") or True
+		opts.jslint_happy = s.get("jslint_happy") or False
+		opts.brace_style = s.get("brace_style") or "collapse"
+		opts.s = s.get("keep_array_indentation") or False
+		opts.indent_level = s.get("indent_level") or 0
 
 		selection = self.view.sel()[0]
 		replaceRegion = selection if len(selection) > 0 else sublime.Region(0, self.view.size())
