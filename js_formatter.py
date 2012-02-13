ea3a891494c67020b90c462b0ccc0a38ae16c88c
Davis Clark <jdc0589@gmail.com>
2012-02-12 19:58:55 -0600

    reposition the cursor where it was prior to the formatting (accounting for whitespace changes, etc...)

--- js_formatter.py
+++ js_formatter.py
@@ -1,4 +1,4 @@
-import sublime, sublime_plugin, jsbeautifier
+import sublime, sublime_plugin, jsbeautifier, re
 
 s = sublime.load_settings("JsFormat.sublime-settings")
 
@@ -6,6 +6,7 @@ class JsFormatCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		settings = self.view.settings()
 
+		# settings
 		opts = jsbeautifier.default_options()
 		opts.indent_char = " " if settings.get("translate_tabs_to_spaces") else "\t"
 		opts.indent_size = int(settings.get("tab_size")) if opts.indent_char == " " else 1 
@@ -17,9 +18,37 @@ class JsFormatCommand(sublime_plugin.TextCommand):
 		opts.indent_level = s.get("indent_level") or 0
 
 		selection = self.view.sel()[0]
+		nwsOffset = self.prev_non_whitespace()		
+
+		# do formatting and replacement
 		replaceRegion = selection if len(selection) > 0 else sublime.Region(0, self.view.size())
 		res = jsbeautifier.beautify(self.view.substr(replaceRegion), opts)
-		prePos = self.view.sel()[0]
 		self.view.replace(edit, replaceRegion, res)
-		self.view.show_at_center(prePos.begin())
-		
+
+		# re-place cursor
+		offset = self.get_nws_offset(nwsOffset, self.view.substr(sublime.Region(0, self.view.size())))
+		rc = self.view.rowcol(offset)
+		pt = self.view.text_point(rc[0], rc[1])
+		sel = self.view.sel()
+		sel.clear()
+		self.view.sel().add(sublime.Region(pt))
+		self.view.show(pt)
+
+
+	def prev_non_whitespace(self):
+		pos = self.view.sel()[0].a
+		preTxt = self.view.substr(sublime.Region(0, pos));
+		return len(re.findall('\S', preTxt))
+
+	def get_nws_offset(self, nonWsChars, buff):
+		nonWsSeen = 0
+		offset = 0
+		for i in range(0, len(buff)):
+			offset += 1
+			if not(buff[i].isspace()):
+				nonWsSeen += 1
+			
+			if(nonWsSeen == nonWsChars):
+				break
+
+		return offset
