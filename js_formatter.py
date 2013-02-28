5fdc3b1f3889fcde478bf65f06d83c38d85c2c48
davis.clark <davis.clark@whiteboard-it.com>
2013-02-28 16:28:20 +0000

    only format on save if the option is enabled AND we are in a js/javascript file.

--- js_formatter.py
+++ js_formatter.py
@@ -16,7 +16,20 @@ s = sublime.load_settings("JsFormat.sublime-settings")
 class PreSaveFormatListner(sublime_plugin.EventListener):
 	"""Event listener to run JsFormat during the presave event"""
 	def on_pre_save(self, view):
-		if(s.get("format_on_save") == True):
+		fName = view.file_name()
+		vSettings = view.settings()
+		syntaxPath = vSettings.get('syntax')
+		syntax = ""
+		ext = ""
+
+		if (fName != None): # file exists, pull syntax type from extension
+			ext = os.path.splitext(fName)[1][1:]
+		if(syntaxPath != None):
+			syntax = os.path.splitext(syntaxPath)[0].split('/')[-1].lower()
+
+		formatFile = "js" in ext or "json" in ext or "javascript" in syntax or "json" in syntax
+
+		if(s.get("format_on_save") == True and formatFile):
 			view.run_command("js_format")
 
 
